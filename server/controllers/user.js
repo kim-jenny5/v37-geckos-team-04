1f4971e5fa862461e05cfde8acd6aa92092d6190
Ezekiel <umanmah234@gmail.com>
2022-02-16 13:00:39 -0500

    fix error message grammar

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,11 +7,11 @@ export const signIn = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    if (!user) return res.status(404).json({ message: 'User not found.' });
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword)
-      return res.status(400).json({ message: 'Invalid Password' });
+      return res.status(400).json({ message: 'Invalid Password.' });
 
     res.status(200).json({ result: user });
   } catch (error) {
@@ -30,7 +30,7 @@ export const signUp = async (req, res) => {
         .json({ message: 'User with this email already exists.' });
 
     if (password !== confirmPassword)
-      return res.status(400).json({ message: 'Passwords do no match. ' });
+      return res.status(400).json({ message: 'Passwords do no match.' });
 
     const hashPassword = bcrypt.hash(password, 12);
 
@@ -42,6 +42,6 @@ export const signUp = async (req, res) => {
 
     res.status(200).json({ result });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: 'Something went wrong.' });
   }
 };
